7b013a3d150a207da292049fdf9879e57f24838e
MrLetsplay <mr.letsplay2003@gmail.com>
2026-08-05 23:33:25 +0200

    Add defaults for stat info, Add resource categories

--- pvp/stats.gd
+++ pvp/stats.gd
@@ -1,3 +1,4 @@
+@tool
 class_name Stats
 extends Resource
 
@@ -10,6 +11,15 @@ enum PVPStat {
 	LUCK,
 }
 
+@export_group("Generation")
 @export var maxPointsPerCategory: int = 10
 @export var totalPoints: int = 30
-@export var info: Dictionary[PVPStat, StatInfo]
+
+@export_group("Info")
+@export var info: Dictionary[PVPStat, StatInfo] = _make_empty()
+
+func _make_empty() -> Dictionary[PVPStat, StatInfo]:
+	var empty_values: Dictionary[PVPStat, StatInfo] = {}
+	for stat: PVPStat in PVPStat.values():
+		empty_values[stat] = StatInfo.new()
+	return empty_values
